1e89f1d316aadcfc54976fdad94f70c20bc21d07
Erik Campobadal <soc@erik.cat>
2017-02-21 08:28:12 +0100

    Tweaks

--- src/Views/settings.blade.php
+++ src/Views/settings.blade.php
@@ -4,17 +4,19 @@
 <div uk-grid>
     <div class="uk-width-1-1@s uk-width-1-5@l"></div>
     <div class="uk-width-1-1@s uk-width-3-5@l">
-        <form method="POST" action="{{ route('laralum::settings.general.update') }}">
+        <form class="uk-form-horizontal" method="POST" action="{{ route('laralum::settings.general.update') }}">
             {{ csrf_field() }}
             <fieldset class="uk-fieldset">
 
                 <div class="uk-margin">
                     <label class="uk-form-label">@lang('laralum_settings::general.appname')</label>
-                    <input value="{{ old('appname', $settings->appname ? $settings->appname : '') }}" name="appname" class="uk-input" type="text" placeholder="@lang('laralum_settings::general.appname_ph')">
-                    <small class="uk-text-meta">@lang('laralum_settings::general.appname_hp')</small>
+                    <div class="uk-form-controls">
+                        <input value="{{ old('appname', $settings->appname ? $settings->appname : '') }}" name="appname" class="uk-input" type="text" placeholder="@lang('laralum_settings::general.appname_ph')">
+                        <small class="uk-text-meta">@lang('laralum_settings::general.appname_hp')</small>
+                    </div>
                 </div>
 
-                <div class="uk-margin">
+                <div class="uk-margin uk-align-right">
                     <button type="submit" class="uk-button uk-button-primary">
                         <span class="ion-forward"></span>&nbsp; @lang('laralum_settings::general.save')
                     </button>
