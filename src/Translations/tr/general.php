987ee2360ea52e3ba43d09c6c2ff5b8eebf1610e
Tanju Yıldız <ben@tanjuyildiz.com>
2018-02-05 01:18:20 +0300

    Added Turkish translations

---
+++ src/Translations/tr/general.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'title'             => 'Uygulama Ayarları',
+    'home'              => 'Ana Sayfa',
+    'subtitle'          => 'Tüm modüllerin ayarlarını tek bir yerde düzenleyin',
+    'modules_settings'  => 'Mevcut Ayarlar',
+    'modules_available' => 'Mevcut Modüller',
+    'save'              => 'Ayarları Kaydet',
+    'updated_settings'  => 'Genel ayarlar güncellendi!',
+    'appname'           => 'Uygulama Adı',
+    'appname_ph'        => 'Uygulamanıza bir ad verin',
+    'appname_hp'        => 'Uygulama adı, uygulamanın her yerinde kullanılacak.',
+    'description'       => 'Meta Açıklaması',
+    'description_ph'    => 'Meta açıklamasını ayarlayın',
+    'description_hp'    => 'Meta açıklaması, yönetim paneli HTML&apos;i içerisinde kullanılacak.',
+    'keywords'          => 'Meta Anahtar Kelimeleri',
+    'keywords_ph'       => 'Meta anahtar kelimelerini ayarlayın',
+    'keywords_hp'       => 'Meta anahtar kelimeleri, yönetim paneli HTML&apos;i içerisinde kullanılacak.',
+    'author'            => 'Meta Yazarı',
+    'author_ph'         => 'Meta yazarını ayarlayın',
+    'author_hp'         => 'Meta yazarı, yönetim paneli HTML&apos;i içerisinde kullanılacak.',
+
+    'unauthorized_action'       => 'Yetkisiz İşlem',
+    'unauthorized_desc'         => 'Genel ayarları güncelleme izniniz yok.',
+    'contact_webmaster'         => 'Güncelleme izniniz olduğunu düşünüyorsanız, lütfen site yöneticisi ile iletişime geçin.',
+];
